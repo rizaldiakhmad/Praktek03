0c7621eae3419350f1096acccd9f2043c7ab29f9
RizaldiAkhmad <32384084+rizaldiakhmad@users.noreply.github.com>
2018-04-10 18:23:20 +0800

    menggunakan method set

--- src/praktek03/KucingAksi.java
+++ src/praktek03/KucingAksi.java
@@ -3,9 +3,9 @@ package praktek03;
 public class KucingAksi {
     public static void main(String[] args) {
         Kucing k = new Kucing();
-        k.jenis= "persia";
-        k.warna="vanila";
-        k.harga=800000;   
+        k.setJenis("persia");
+        k.setWarna("vanila");
+        k.setHarga(800000);   
         
         k.cetakinfo();
         
