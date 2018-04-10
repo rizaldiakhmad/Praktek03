d48b49d5ec0ee7e8d08bc763bcafb8d0443ad7cf
RizaldiAkhmad <32384084+rizaldiakhmad@users.noreply.github.com>
2018-04-10 18:18:59 +0800

    buat Getter and Setter

--- src/praktek03/Kucing.java
+++ src/praktek03/Kucing.java
@@ -1,12 +1,37 @@
 package praktek03;
 
 public class Kucing {
-    String jenis;
-    String warna;
-    double harga;
+    private String jenis;
+    private String warna;
+    private double harga;
     
     void cetakinfo(){ 
     System.out.println("Jenis kucing \t : "+jenis+"\n"+
                 "warna kucing \t : "+warna+"\n"+"Harga kucing \t : "+harga);
 }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
 }
