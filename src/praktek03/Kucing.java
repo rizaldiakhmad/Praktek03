ad341a80c5edcdf26508970a63e5682321cc8e6d
RizaldiAkhmad <32384084+rizaldiakhmad@users.noreply.github.com>
2018-04-10 18:09:38 +0800

    membuat method cetakinfo()

--- src/praktek03/Kucing.java
+++ src/praktek03/Kucing.java
@@ -1,7 +1,12 @@
 package praktek03;
 
 public class Kucing {
-    String Jenis;
+    String jenis;
     String warna;
     double harga;
+    
+    void cetakinfo(){ 
+    System.out.println("Jenis kucing \t : "+jenis+"\n"+
+                "warna kucing \t : "+warna+"\n"+"Harga kucing \t   : "+harga);
+}
 }
