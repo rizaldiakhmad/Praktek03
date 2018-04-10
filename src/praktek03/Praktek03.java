32b6224e9431a7d37aa4292b12fdbc49ae807a7b
RizaldiAkhmad <32384084+rizaldiakhmad@users.noreply.github.com>
2018-04-10 17:59:42 +0800

    Initial commit

---
+++ src/praktek03/Praktek03.java
@@ -0,0 +1,12 @@
+package praktek03;
+
+public class Praktek03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
